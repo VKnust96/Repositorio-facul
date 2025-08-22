711be091ab61ea86154a100c08f69494bb5fa0c1
VKnust96 <vitorknust1996@gmail.com>
2025-08-22 16:38:42 -0300

    Desafio aventureiro super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -9,7 +9,7 @@ int main() {
     float area1;
     float pib1;
     int pontos_turisticos1;
-    float densidade_populacional1;
+    float densidade_populacional1; 
     float pib_per_capita1;
 
     // Declaração de variáveis para a Carta 2
@@ -68,3 +68,47 @@ int main() {
 
     printf("Digite a Area (em km2): ");
     scanf("%f", &area2);
+
+    printf("Digite o PIB (em bilhoes de reais): ");
+    scanf("%f", &pib2);
+
+    printf("Digite o numero de Pontos Turisticos: ");
+    scanf("%d", &pontos_turisticos2);
+
+    // --- Cálculo dos novos atributos para a Carta 2
+    densidade_populacional2 = (float)populacao2 / area2;
+    pib_per_capita2 = (pib2 * 1000000000) / populacao2;
+
+    printf("\n");
+
+    // --- Exibição das Cartas ---
+    printf("--- Cartas Cadastradas ---\n\n");
+
+    // Exibição da Carta 1
+    printf("Carta 1:\n");
+    printf("Estado: %s\n", estado1);
+    printf("Codigo: %s\n", codigo_carta1);
+    printf("Nome da Cidade: %s\n", nome_cidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km2\n", area1);
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos1);
+    printf("Densidade Populacional: %.2f hab/km2\n", densidade_populacional1);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita1);
+
+    printf("\n");
+
+    // Exibição da Carta 2
+    printf("Carta 2:\n");
+    printf("Estado: %s\n", estado2);
+    printf("Codigo: %s\n", codigo_carta2);
+    printf("Nome da Cidade: %s\n", nome_cidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km2\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos2);
+    printf("Densidade Populacional: %.2f hab/km2\n", densidade_populacional2);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita2);
+
+    return 0;
+}
